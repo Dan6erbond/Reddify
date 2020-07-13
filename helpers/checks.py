0222fa13eb764185d82fb5d075cf4c724027107a
Dan6erbond <moravrav@gmail.com>
2020-07-13 10:09:30 +0200

    Add advanced_user check

--- helpers/checks.py
+++ helpers/checks.py
@@ -1,11 +1,16 @@
 from discord.ext import commands
 
+from const import ADVANCED_USERS
 from database.database import session
 from database.models import DiscordUser, RedditUser
 
 
-async def is_verified(ctx):
+async def is_verified(ctx: commands.Context):
     if session.query(DiscordUser).join(
             RedditUser).filter(DiscordUser.user_id == ctx.author.id).first():
         return True
     return False
+
+
+async def advanced_user(ctx: commands.Context):
+    return ctx.author.id in ADVANCED_USERS
