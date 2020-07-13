a0601f34dfd4e2ff4656ab219fee5a3d9a03c73c
Dan6erbond <moravrav@gmail.com>
2020-07-13 11:03:35 +0200

    Update formatting

--- database/database.py
+++ database/database.py
@@ -1,9 +1,11 @@
+import pathlib
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import backref, relationship, sessionmaker
-import pathlib
+
 from .base import Base
-from .models.reddit_user import RedditUser
 from .models.discord_user import DiscordUser
+from .models.reddit_user import RedditUser
 
 path = pathlib.Path(__file__).parent.absolute()
 engine = create_engine(f'sqlite:///{path}/database.db')
